18009503418e2f19f6ffd39ddee8d05da589832e
Elia Schelokov <thaumant@gmail.com>
2015-03-19 18:05:36 +0300

    Make small fixes in ClassTransformer

--- src/ClassTransformer.js
+++ src/ClassTransformer.js
@@ -12,18 +12,20 @@ module.exports = class ClassTransformer extends UnitTransformer {
 
         let encode  = spec.encode
         switch (true) {
-            case typeof encode !== 'function': this.encode = encode
-            case typeof encode !== 'string':   this.encode = (val) => val[encode]()
+            case typeof encode === 'function': this.encode = encode; break
+            case typeof encode === 'string':   this.encode = (val) => val[encode](); break
             default:                           this.encode = (val) => val.toJSON()
         }
     }
 
     validateSpec(s) {
         let err = super.validateSpec(s)
-        if (err)                                                 return err
+        if (err) return err
+
         let tokenIsString = typeof s.token === 'string',
             maybeToken = (tokenIsString && s.token) || (s.class && s.class.name),
             forToken = maybeToken ? ` for ${maybeToken}` : ''
+
         if (s.token && !tokenIsString)                           return `invalid token${forToken}`
         if ((typeof s.class !== 'function') || !s.class.name)    return `invalid class${forToken}`
         if (s.decode && (typeof s.decode !== 'function'))        return `invalid decoder${forToken}`
