6d70a2f87b06757c352952263d69a29ccdf3d4a6
Elia Schelokov <thaumant@gmail.com>
2015-03-21 00:36:03 +0300

    Improve tests for CompositeTransformer constructor and validators

--- tests/CompositeTransformer.js
+++ tests/CompositeTransformer.js
@@ -13,36 +13,14 @@ describe('CompositeTransformer', () => {
         let make = CompositeTransformer.prototype.makeUnitTransformer
 
         it('accepts spec and returns correspondent UnitTransformer', () => {
-            let t1 = {class: Bar},
-                t2 = {token: 'Foo', pred: isFoo, encode: fooEnc, decode: fooDec}
-            assert.instanceOf(make(t1), ClassTransformer)
-            assert.instanceOf(make(t2), PredicateTransformer)
-        })
-
-    })
-
-    describe('#constructor()', () => {
-
-        it('stores prefix and serializer options if provided', () => {
-            let t = new CompositeTransformer([], {prefix: 'foo', serializer: isFoo})
-            assert.strictEqual('foo', t.options.prefix)
-            assert.strictEqual(isFoo, t.options.serializer)
-        })
-
-        it('throws an error if specs is not an array', () => {
-            let test1 = () => new CompositeTransformer(3),
-                test2 = () => new CompositeTransformer({})
-            assert.throw(test1, 'Expected array of specs')
-            assert.throw(test2, 'Expected array of specs')
-        })
-
-        it('creates array of UnitTransformer instances from array of specs', () => {
             let spec1 = {class: Bar},
-                spec2 = {token: 'Foo', pred: isFoo, encode: fooEnc, decode: fooDec},
-                t = new CompositeTransformer([spec1, spec2])
-            assert.lengthOf(t.transformers, 2)
-            assert.instanceOf(t.transformers[0], ClassTransformer)
-            assert.instanceOf(t.transformers[1], PredicateTransformer)
+                spec2 = {token: 'Foo', pred: isFoo, encode: fooEnc, decode: fooDec}
+            assert.instanceOf(make(spec1), ClassTransformer)
+            assert.instanceOf(make(spec2), PredicateTransformer)
+        })
+
+        it('throws an error if cannot determine which transformer to create', () => {
+            assert.throws(() => make({}), 'Invalid spec, no class or predicate')
         })
 
     })
@@ -71,6 +49,44 @@ describe('CompositeTransformer', () => {
                 spec2 = {token: 'Bar', class: Bar, encode: barEnc},
                 ts = [spec1, spec2].map(make)
             assert.strictEqual(undefined, val(ts))
+        })
+
+    })
+
+    describe('#constructor()', () => {
+
+        it('stores prefix and serializer options if provided', () => {
+            let t = new CompositeTransformer([], {prefix: 'foo', serializer: isFoo})
+            assert.strictEqual('foo', t.options.prefix)
+            assert.strictEqual(isFoo, t.options.serializer)
+        })
+
+        it('throws an error if specs is not an array', () => {
+            let test1 = () => new CompositeTransformer(3),
+                test2 = () => new CompositeTransformer({})
+            assert.throw(test1, 'Expected array of specs')
+            assert.throw(test2, 'Expected array of specs')
+        })
+
+        it('throws if some spec is not valid', () => {
+            let test = () => new CompositeTransformer([{pred: isFoo}])
+            assert.throw(test, 'Failed to create predicate transformer: missing token')
+        })
+
+        it('performs #validateConsistency() on transformers made of specs', () => {
+            let spec1 = {class: Bar},
+                spec2 = {class: Bar},
+                test = () => new CompositeTransformer([spec1, spec2])
+            assert.throw(test, 'Inconsistent transformers: 2 transformers for token Bar')
+        })
+
+        it('creates array of UnitTransformer instances from array of valid specs', () => {
+            let spec1 = {class: Bar},
+                spec2 = {token: 'Foo', pred: isFoo, encode: fooEnc, decode: fooDec},
+                t = new CompositeTransformer([spec1, spec2])
+            assert.lengthOf(t.transformers, 2)
+            assert.instanceOf(t.transformers[0], ClassTransformer)
+            assert.instanceOf(t.transformers[1], PredicateTransformer)
         })
 
     })
