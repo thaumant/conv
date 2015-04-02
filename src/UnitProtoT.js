477e7766c7890bf8a45937f3eefbc1cab41de3b0
Elia Schelokov <thaumant@gmail.com>
2015-04-03 02:51:33 +0300

    Make unit prototype transfomer class

---
+++ src/UnitProtoT.js
@@ -0,0 +1,68 @@
+const UnitT = require('./UnitT'),
+    {getProtoChain, isStr, isFunc} = require('./util')
+
+
+module.exports = class UnitProtoT extends UnitT {
+    constructor(spec) {
+        let err = this.validateSpec(spec)
+        if (err) throw new Error(`Failed to create proto transformer: ${err}`)
+
+        this.proto      = spec.proto
+        this.token      = spec.token
+        this.namespace  = spec.namespace
+        this.path       = (this.namespace ? this.namespace + '.' : '') + this.token
+        this.protoChain = getProtoChain(this.proto, true)
+
+        this.restore    = spec.restore || (dumped) => {
+            let result = Object.create(this.proto)
+            for (let key in dumped) result[key] = dumped[key]
+        }
+
+        let dump  = spec.dump
+        switch (true) {
+            case typeof dump === 'function': this.dump = dump; break
+            case typeof dump === 'string':   this.dump = (val) => val[dump](); break
+            default: this.dump = (val) => {
+                let result = {}
+                for (let key in val) {
+                    if (val.hasOwnProperty(key)) result[key] = val[key]
+                }
+                return result
+            }
+        }
+    }
+
+    validateSpec(s) {
+        if (s instanceof UnitProtoT) return
+        let err = super.validateSpec(s)
+        if (err) return err
+
+        switch (true) {
+            case s.token == null:                 return 'missing token'
+            case this.isValidName(token):         break
+            default:                              return 'invalid token'
+        }
+        switch (true) {
+            case s.namespace == null:             break
+            case this.isValidNS(s.namespace):     break
+            default:                              return `invalid namespace for ${token}`
+        }
+        switch (true) {
+            case s.proto == null:                 break
+            case typeof s.proto === 'object':     break
+            case typeof s.proto === 'function':   break
+            default:                              return `invalid proto for ${token}`
+        }
+        switch (true) {
+            case s.restore == null:               break
+            case typeof s.restore === 'function': break
+            default:                              return `invalid restore method for ${token}`
+        }
+        switch (true) {
+            case !s.dump:                         break
+            case typeof s.dump === 'function':    break
+            case typeof s.dump === 'string':      break
+            default:                              return `invalid dump method for ${token}`
+        }
+    }
+}
